50182c4218b35778c66939f931355e0e398eb8fe
Anis Merchant <anisxmerchant@gmail.com>
2026-01-29 12:16:44 -0500

    feat: add vpc and public subnet networking foundation

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,24 @@
 provider "aws" {
   region = var.aws_region
 }
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "infrapro-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.aws_region}a"
+
+  tags = {
+    Name = "infrapro-public-subnet"
+  }
+}
